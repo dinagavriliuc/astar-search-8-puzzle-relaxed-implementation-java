8873ba0f70ae10063e7ad2d4f18ca1c061990427
bit0104@iit.du.ac.bd <bit0104@iit.du.ac.bd@db6b5571-80b5-95c8-cba6-77f6807a1fc4>
2012-02-02 06:17:13 +0000

    open nodes

---
+++ trunk/stateCollection/OpenNodes.java
@@ -0,0 +1,22 @@
+package stateCollections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import buildNodes.State;
+
+public class OpenNodes{
+
+public ArrayList<State> openNodesList;
+
+
+public OpenNodes() {
+	// TODO Auto-generated constructor stub
+	openNodesList = new ArrayList<State>();
+	
+}
+	
+}
