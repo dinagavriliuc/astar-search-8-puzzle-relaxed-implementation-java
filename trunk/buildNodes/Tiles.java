13f8fce365d6223bb7a4719b1d1cdbb6cfaffb0d
bit0104@iit.du.ac.bd <bit0104@iit.du.ac.bd@db6b5571-80b5-95c8-cba6-77f6807a1fc4>
2012-02-02 06:14:11 +0000

    Tiles class

---
+++ trunk/buildNodes/Tiles.java
@@ -0,0 +1,54 @@
+package buildNodes;
+
+public class Tiles {
+
+	String name;
+	ArrayIndex currentPosition;
+	ArrayIndex optimalPosition;
+
+	public Tiles(String name, ArrayIndex currentPosition, ArrayIndex optimalPosition) {
+		this.name = name;
+		this.currentPosition = currentPosition;
+		this.optimalPosition = optimalPosition;
+	}
+	public Tiles(String name, ArrayIndex optimalPosition) {
+		this.name = name;
+		this.optimalPosition = optimalPosition;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayIndex getCurrentPosition() {
+		return currentPosition;
+	}
+
+	public void setCurrentPosition(ArrayIndex currentPosition) {
+		this.currentPosition = currentPosition;
+	}
+
+	public ArrayIndex getOptimalPosition() {
+		return optimalPosition;
+	}
+
+	public void setOptimalPosition(ArrayIndex optimalPosition) {
+		this.optimalPosition = optimalPosition;
+	}
+	
+	public int DistanceOfTwoTiles(ArrayIndex one, ArrayIndex two){
+		
+		return Math.abs(one.column - two.column) + Math.abs(one.row - two.row);
+		
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return name.equals(((Tiles)obj).name);
+	}
+
+}
