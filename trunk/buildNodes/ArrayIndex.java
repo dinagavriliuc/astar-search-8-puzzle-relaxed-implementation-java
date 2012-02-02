ac7e91c23395c2ef3224c6db694da6bd200ae8f1
bit0104@iit.du.ac.bd <bit0104@iit.du.ac.bd@db6b5571-80b5-95c8-cba6-77f6807a1fc4>
2012-02-02 06:13:21 +0000

    Class for array indexing

---
+++ trunk/buildNodes/ArrayIndex.java
@@ -0,0 +1,29 @@
+package buildNodes;
+
+public class ArrayIndex {
+	int row;
+	int column;
+	
+	public ArrayIndex(int row, int column) {
+		// TODO Auto-generated constructor stub
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+}
