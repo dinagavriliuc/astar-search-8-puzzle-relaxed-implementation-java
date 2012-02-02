f7b124a7d7703bc5a194f7364fe20ce7006e8bd2
bit0104@iit.du.ac.bd <bit0104@iit.du.ac.bd@db6b5571-80b5-95c8-cba6-77f6807a1fc4>
2012-02-02 06:16:58 +0000

    closed nodes

---
+++ trunk/stateCollection/ClosedNodes.java
@@ -0,0 +1,21 @@
+package stateCollections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import buildNodes.*;
+
+
+
+public class ClosedNodes {
+	
+	public ArrayList<State> closedNodesList;
+	
+	public ClosedNodes() {
+		// TODO Auto-generated constructor stub
+		closedNodesList = new ArrayList<State>();
+	}
+}
