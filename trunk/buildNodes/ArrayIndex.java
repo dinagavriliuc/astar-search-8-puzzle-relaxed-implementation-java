d18b0598780429b2d1e6f4bf2f644f8fc869d383
bit0104@iit.du.ac.bd <bit0104@iit.du.ac.bd@db6b5571-80b5-95c8-cba6-77f6807a1fc4>
2012-02-02 06:18:26 +0000

    ..

--- trunk/buildNodes/ArrayIndex.java
+++ trunk/buildNodes/ArrayIndex.java
@@ -1,29 +1,29 @@
 package buildNodes;
 
 public class ArrayIndex {
-	int row;
-	int column;
-	
-	public ArrayIndex(int row, int column) {
-		// TODO Auto-generated constructor stub
-		this.row = row;
-		this.column = column;
-	}
+    int row;
+    int column;
+    
+    public ArrayIndex(int row, int column) {
+        // TODO Auto-generated constructor stub
+        this.row = row;
+        this.column = column;
+    }
 
-	public int getRow() {
-		return row;
-	}
+    public int getRow() {
+        return row;
+    }
 
-	public void setRow(int row) {
-		this.row = row;
-	}
+    public void setRow(int row) {
+        this.row = row;
+    }
 
-	public int getColumn() {
-		return column;
-	}
+    public int getColumn() {
+        return column;
+    }
 
-	public void setColumn(int column) {
-		this.column = column;
-	}
+    public void setColumn(int column) {
+        this.column = column;
+    }
 
 }
